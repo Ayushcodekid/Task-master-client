f9835dd2a5ce8a71451b562e4be81f09149acfed
Ayushcodekid <ayush0101works@gmail.com>
2024-11-12 12:29:32 +0530

    api url changed

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL= 'https://task-master-1-ei2r.onrender.com';
+const API_URL= 'https://task-master-2.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_URL
